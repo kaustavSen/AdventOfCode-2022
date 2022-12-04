cc2573ad9ba42760822a43f832dec0701342f4c7
Kaustav Sen <sen_kaustav@hotmail.com>
2022-12-04 18:47:43 +0000

    day 4 code edits

--- day_04.py
+++ day_04.py
@@ -10,23 +10,19 @@ def get_units(elf_alloc):
 def is_fully_contained(units1, units2):
     return units1.issuperset(units2) or units2.issuperset(units1)
 
-num_fully_contained = 0
-for elf1, elf2 in data:
-    elf1_units = get_units(elf1)
-    elf2_units = get_units(elf2)
-    num_fully_contained += is_fully_contained(elf1_units, elf2_units)
-
-# part 1 solution
-print(num_fully_contained)
-
 def is_disjoint(units1, units2):
     return units1.isdisjoint(units2)
 
+num_fully_contained = 0
 num_disjoint = 0
 for elf1, elf2 in data:
     elf1_units = get_units(elf1)
     elf2_units = get_units(elf2)
+    num_fully_contained += is_fully_contained(elf1_units, elf2_units)
     num_disjoint += is_disjoint(elf1_units, elf2_units)
+
+# part 1 solution
+print(num_fully_contained)
 
 # part 2 solution
 print(len(data) - num_disjoint)
